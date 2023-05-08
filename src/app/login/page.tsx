0ee2ce98afc8869388c312c37678dab3c9305de3
Nadav Shor <nadavshordev@gmail.com>
2023-05-09 01:49:46 +0300

    added favicon and started working on a basic navbar

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+    title: 'Notes App | Login',
+    description: 'login page',
+}
+
+export default function Login() {
+
+  return (
+    <h1>Login</h1>
+  )
+}
---
+++ src/components/AppNavbar.tsx
@@ -0,0 +1,8 @@
+import styles from '../app/styles/mainlayoutstyles.module.css'
+
+export default function AppNavbar() {
+
+  return (
+    <nav className={styles.navbarContainer}>AppNavbar</nav>
+  )
+}
