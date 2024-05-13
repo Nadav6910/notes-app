b523bb32ff82bc79eeeaa7fcbd79d53e7f32d233
Nadav Shor <nadavshordev@gmail.com>
2024-05-13 18:08:02 +0300

    small css fix

--- src/components/create-notes-page-components/NoteTypeSelector.tsx
+++ src/components/create-notes-page-components/NoteTypeSelector.tsx
@@ -1,56 +1,61 @@
 'use client'
 
 import styles from "../../app/my-notes/create/styles/createNote.module.css"
+import { useState } from "react";
 import MotionWrap from "@/wrappers/MotionWrap";
 import { useAnimationControls } from "framer-motion"
 import { NoteTypeSelectorProps } from "../../../types";
 
 export default function NoteTypeSelector({createdNoteType}: NoteTypeSelectorProps) {
 
-  const controls = useAnimationControls()
+    const [selectedNoteType, setSelectedNoteType] = useState("Items list")
 
-  const toggleButton = (e: React.MouseEvent) => {
+    const controls = useAnimationControls()
 
-    if (e.currentTarget.childNodes[0].textContent === "Items list") {
-        controls.start({x: 0})
-        createdNoteType("Items list")
+    const toggleButton = (e: React.MouseEvent) => {
+
+        if (e.currentTarget.childNodes[0].textContent === "Items list") {
+            controls.start({left: "0.5em"})
+            setSelectedNoteType("Items list")
+            createdNoteType("Items list")
+        }
+
+        if (e.currentTarget.childNodes[0].textContent === "Note book") {
+            controls.start({left: "6.75em"})
+            setSelectedNoteType("Note book")
+            createdNoteType("Note book")
+        }
     }
+    
+    return (
 
-    if (e.currentTarget.childNodes[0].textContent === "Note book") {
-        controls.start({x: 100})
-        createdNoteType("Note book")
-    }
-  }
-  
-  return (
-
-    <div className={styles.noteTypeSelectorContainer}>
-        <MotionWrap
-            className={styles.typeSelectionArea}
-            animate={controls}       
-            transition={{ type: "spring", stiffness: 200, damping: 20, duration: 0.5, bounce: 0.2}}
-        />
-        <div 
-            onClick={toggleButton} 
-            className={styles.btnWrapper}
-        >
-            <button  
-                className={styles.typeSelectorBtn}
+        <div className={styles.noteTypeSelectorContainer}>
+            <MotionWrap
+                className={selectedNoteType === "Items list" ? styles.typeSelectionAreaList : styles.typeSelectionAreaBook}
+                animate={controls}       
+                transition={{ type: "spring", stiffness: 160, damping: 19, duration: 0.5, bounce: 0.2}}
+            />
+            <div 
+                onClick={toggleButton} 
+                className={styles.btnWrapper}
             >
-                Items list
-            </button>
-        </div>
+                <button  
+                    className={styles.typeSelectorBtn}
+                >
+                    Items list
+                </button>
+            </div>
 
-        <div 
-            onClick={toggleButton} 
-            className={styles.btnWrapper}
-        >
-            <button 
-                className={styles.typeSelectorBtn}
+            <div 
+                onClick={toggleButton} 
+                className={styles.btnWrapper}
             >
-                Note book
-            </button>
+                <button 
+                    className={styles.typeSelectorBtn}
+                >
+                    Note book
+                </button>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
