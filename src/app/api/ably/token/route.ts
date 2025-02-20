78e3f26d8c04884072cfe36e3a63d82ced34f3a8
Nadav Shor <nadavshordev@gmail.com>
2025-02-20 16:25:35 +0200

    added security update for the ably token route

--- src/app/api/ably/token/route.ts
+++ src/app/api/ably/token/route.ts
@@ -1,11 +1,20 @@
 import { NextResponse } from 'next/server'
 import Ably from 'ably'
+import { getServerSession } from "next-auth/next"
+import { authOptions } from '../../auth/[...nextauth]/options'
 
 export async function GET(request: Request) {
+
+  // Verify that the user is authenticated
+  const session = await getServerSession(authOptions)
+  if (!session) {
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
+  }
+  
   // Extract clientId from query parameters if provided
   const { searchParams } = new URL(request.url)
   const clientId = searchParams.get('clientId') || undefined
-
+  
   // Verify that your secret API key is set
   if (!process.env.ABLY_API_KEY) {
     console.error('Missing ABLY_API_KEY env variable')
@@ -18,7 +27,9 @@ export async function GET(request: Request) {
     // Create token request with the optional clientId
     const tokenRequest = await ably.auth.createTokenRequest({ clientId })
     return NextResponse.json(tokenRequest)
-  } catch (error: any) {
+  } 
+  
+  catch (error: any) {
     console.error('Error generating token:', error)
     return NextResponse.json({ error: error.message }, { status: 500 })
   }
